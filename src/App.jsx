ff999565b888b6ece1e67625fcd3bbe0081dbb96
The Margad <122777402+TheMargadXR@users.noreply.github.com>
2024-02-04 22:39:25 +0800

    ux ui update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 import React from "react";
-// import Home from "./Pages/Hero";
-import List from "./Pages/List";
+import Hero from "./Pages/Hero";
+import List from "./Pages/Lists/List";
+import List1 from "./Pages/Lists/List1";
+import List2 from "./Pages/Lists/List2";
 import Footer from "./Pages/Footer";
+import Label from "./Pages/Label";
 function App() {
   return (
     <>
+      <Hero />
+      <Label />
+      <br></br>
       <List />
+      <br />
+      <List1 />
+      <br />
+      <List2 />
       <Footer />
     </>
   );
---
+++ src/Pages/Lists/List2.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import testData from "../../Data/testData2.json";
+
+export default function Example() {
+  return (
+    <div>
+      <div role="list" className="divide-y bg-gray-100  p-10 rounded-3xl">
+        <h2 className="text-3xl font-bold py-3 text-gray-800 mb-4">
+          ЭЛСЭЛТИЙН ЕРӨНХИЙ ШАЛГАЛТ
+        </h2>
+        {testData.map((data) => (
+          <div
+            key={data.about}
+            className="flex justify-between gap-x-6 p-3 mb-5 shadow-md rounded-3xl bg-indigo-50"
+          >
+            <p className="rounded-2xl bg-indigo-600 p-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              {data.class}-р Англи
+            </p>
+            <div className="flex min-w-0 gap-x-4 ">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {data.name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {data.about}
+                </p>
+              </div>
+            </div>
+            <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
+              <button className="rounded-2xl bg-indigo-600 p-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <a href={data.courseUrl}>Үзэх</a>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
